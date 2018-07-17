0a431bc5c792cdacf27c20011280419044d34f21
Training_B8C.07.19 <Training_B8C.07.19@BDC8-DX-41FRX52.dir.svc.accenture.com>
2018-07-17 12:54:17 +0530

    Test

--- SeleniumWebdriver/src/test/java/com/test/Testing.java
+++ SeleniumWebdriver/src/test/java/com/test/Testing.java
@@ -21,7 +21,7 @@ public class Testing {
 		
 		Assert.assertEquals(title, "Sign in to GitHub · GitHub");
 		
-		System.out.println("Success - Output Verif");
+		System.out.println("Success - Output Veri");
 	}
 	}
 
